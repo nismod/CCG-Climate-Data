9a4c99afb10a7796779a7ba3d6da9cb22e2dbb8a
Tom Russell <tom.russell@ouce.ox.ac.uk>
2025-09-15 11:58:15 +0100

    sbatch scripts for rechunk

---
+++ rechunk_tasmax.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=10G
+#SBATCH --time=01:20:00
+#SBATCH --partition=Short
+#SBATCH --array=0-145
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+export MAMBA_EXE='/lustre/soge1/users/mert2014/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/lustre/soge1/users/mert2014/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
+
+date
+hostname
+pwd
+
+micromamba activate heat-analysis
+python 1_rechunk_var.py /ouce-home/projects/mistral/ccg-2025-hazards/ /ouce-home/data/incoming/NEX-GDDP-CMIP6/ tasmax $SLURM_ARRAY_TASK_ID
+
+date
+echo "Done."
